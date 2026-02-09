eb6e092821944d95a08a5009e487bb85d0daa0b8
laurent3010 <l4ur3ntb34upr3@gmail.com>
2026-02-09 10:15:03 -0500

    adition de la page login sign up en visuel creation d'une pige wild card et ajout des routes pour ces trois pages

--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Login } from './component/login/login';
+import { Signup } from './component/signup/signup'; 
+import { Notfound } from './component/notfound/notfound';
+import path from 'path';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+{
+    path: '',
+    component: Login
+},
+{
+    path: 'signup',
+    component: Signup
+
+},
+{
+    path: '**',
+    component: Notfound
+}
+
+
+];

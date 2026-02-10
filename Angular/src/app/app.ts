f408a447d75323264bd9c3901d98334a2ed2eb85
GabrielBolduc <bolducGabriel@outlook.com>
2026-02-10 16:50:16 -0500

    modifications de la structure des fichiers

--- Angular/src/app/app.ts
+++ Angular/src/app/app.ts
@@ -3,40 +3,25 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { AuthService } from './auth.service';
+import { AuthService } from '../app/core/services/auth.service';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterLink, 
-    RouterOutlet, 
-    MatToolbarModule, 
-    MatButtonModule, 
-    MatIconModule,
-    TranslateModule
-  ],
+  standalone: true,
+  imports: [RouterOutlet], 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Angular');
-  public auth = inject(AuthService);
-  public translate = inject(TranslateService);
+  private translate = inject(TranslateService);
 
   constructor() {
     this.translate.addLangs(['en', 'fr']);
-    this.translate.setFallbackLang('en');
+    this.translate.setDefaultLang('en');
     
     const browserLang = this.translate.getBrowserLang();
     this.translate.use(browserLang?.match(/en|fr/) ? browserLang : 'en');
-  }
-
-  toggleLanguage() {
-    const current = this.translate.getCurrentLang(); 
-    const targetLang = current === 'en' ? 'fr' : 'en';
-    
-    this.translate.use(targetLang);
   }
 }

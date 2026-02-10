1857b787d41f91545c3935ba22bd6e790c78e73b
lizencerfalia <lizencerfalia@gmail.com>
2026-02-10 11:53:46 -0500

    localisation (header, login et sign up)

--- Angular/src/app/app.ts
+++ Angular/src/app/app.ts
@@ -4,6 +4,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { AuthService } from './auth.service';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 
 @Component({
@@ -13,12 +14,29 @@ import { AuthService } from './auth.service';
     RouterOutlet, 
     MatToolbarModule, 
     MatButtonModule, 
-    MatIconModule
+    MatIconModule,
+    TranslateModule
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Angular');
-  public auth = inject(AuthService); 
+  public auth = inject(AuthService);
+  public translate = inject(TranslateService);
+
+  constructor() {
+    this.translate.addLangs(['en', 'fr']);
+    this.translate.setFallbackLang('en');
+    
+    const browserLang = this.translate.getBrowserLang();
+    this.translate.use(browserLang?.match(/en|fr/) ? browserLang : 'en');
+  }
+
+  toggleLanguage() {
+    const current = this.translate.getCurrentLang(); 
+    const targetLang = current === 'en' ? 'fr' : 'en';
+    
+    this.translate.use(targetLang);
+  }
 }
--- Angular/src/app/component/login/login.ts
+++ Angular/src/app/component/login/login.ts
@@ -7,17 +7,28 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from "@angular/router";
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.html',
   styleUrls: ['./login.css'],
-  imports: [RouterLink,MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule, MatIconModule, MatCardModule]
+  imports: [
+    RouterLink,
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatButtonModule, 
+    ReactiveFormsModule, 
+    MatIconModule, 
+    MatCardModule,
+    TranslateModule
+  ]
+
 })
 export class Login {
   hidePassword = true;
 
-  constructor() {}
+  constructor(private translate: TranslateService) {}
   
   
   

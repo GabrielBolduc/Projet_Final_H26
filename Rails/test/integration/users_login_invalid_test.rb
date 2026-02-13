da0d1d72688e111ca437244f551772da809bce3a
GabrielBolduc <bolducGabriel@outlook.com>
2026-02-12 19:50:44 -0500

    test complet

--- Rails/test/integration/users_login_invalid_test.rb
+++ Rails/test/integration/users_login_invalid_test.rb
@@ -12,20 +12,92 @@ class UsersLoginInvalidTest < ActionDispatch::IntegrationTest
     )
   end
 
-  test "should not login with invalid credentials" do
+  # mauvais mdp
+  test "should not login with invalid password" do
+    # modif ou non
     assert_no_difference("User.count") do
       post user_session_url, params: {
         user: {
           email: @user.email,
-          password: "WRONG_PASSWORD"
+          password: "asdfgh"
         }
       }, as: :json
     end
 
+    # code http
     assert_response :success 
 
+    # format reponse
     json = JSON.parse(response.body)
 
+    # donne reponse
+    assert_equal "error", json["status"]
+    assert_equal 401, json["code"]
+  end
+
+  # faux email
+  test "should not login with non-existent email" do
+    # modif ou non
+    assert_no_difference("User.count") do
+      post user_session_url, params: {
+        user: {
+          email: "unknown@test.com",
+          password: "qwerty"
+        }
+      }, as: :json
+    end
+
+    # code http
+    assert_response :success 
+
+    # format reponse
+    json = JSON.parse(response.body)
+
+    # donne reponse
+    assert_equal "error", json["status"]
+    assert_equal 401, json["code"]
+  end
+
+  # login sans email
+  test "should not login with missing email" do
+    # modif ou non
+    assert_no_difference("User.count") do
+      post user_session_url, params: {
+        user: {
+          password: "qwerty"
+        }
+      }, as: :json
+    end
+
+    # code http
+    assert_response :success 
+
+    # format reponse
+    json = JSON.parse(response.body)
+
+    # donne reponse
+    assert_equal "error", json["status"]
+    assert_equal 401, json["code"]
+  end
+
+  # login sans mdp
+  test "should not login with missing password" do
+    # modif ou non
+    assert_no_difference("User.count") do
+      post user_session_url, params: {
+        user: {
+          email: @user.email
+        }
+      }, as: :json
+    end
+
+    # code http
+    assert_response :success 
+
+    # format reponse
+    json = JSON.parse(response.body)
+
+    # donne reponse
     assert_equal "error", json["status"]
     assert_equal 401, json["code"]
   end
--- Rails/test/integration/users_signup_invalid_test.rb
+++ Rails/test/integration/users_signup_invalid_test.rb
@@ -1,10 +1,23 @@
 require "test_helper"
 
 class UsersSignupInvalidTest < ActionDispatch::IntegrationTest
+ 
+  setup do
+    @existing_user = User.create!(
+      email: "existant@test.com",
+      password: "qwerty",
+      password_confirmation: "qwerty",
+      name: "NOM",
+      role: "CLIENT",
+      phone_number: "111-111-1111"
+    )
+  end
+
+  # nom, role, phone_number manquant
   test "should fail signup with missing info" do
+    # modif ou non
     assert_no_difference("User.count") do
       post user_registration_url, params: {
-        # manque name role phone_number
         user: {
           email: "fail@user.com",
           password: "qwerty",
@@ -13,13 +26,72 @@ class UsersSignupInvalidTest < ActionDispatch::IntegrationTest
       }
     end
 
+    # code http
     assert_response :success 
 
+    # format reponse
     assert_match "application/json", response.media_type
     json = JSON.parse(response.body)
 
+    # donne repomse
     assert_equal "error", json["status"]
     assert_equal 422, json["code"] 
     assert_includes json["errors"].to_s, "Name can't be blank"
+  end
+
+  # confirm mdp incorrect
+  test "should fail signup with password mismatch" do
+    # modif ou non
+    assert_no_difference("User.count") do
+      post user_registration_url, params: {
+        user: {
+          email: "mismatch@test.com",
+          name: "Mismatch Test",
+          role: "CLIENT",
+          phone_number: "555-555-5555",
+          password: "qwerty",
+          password_confirmation: "asdfgh" 
+        }
+      }
+    end
+
+    # code http
+    assert_response :success
+
+    # format reponse
+    json = JSON.parse(response.body)
+
+    # donne reponse
+    assert_equal "error", json["status"]
+    assert_equal 422, json["code"]
+    assert_includes json["errors"].to_s, "Password confirmation doesn't match Password"
+  end
+
+  # email utiliser
+  test "should fail signup with taken email" do
+    # modif ou non
+    assert_no_difference("User.count") do
+      post user_registration_url, params: {
+        user: {
+          email: @existing_user.email, 
+          name: "Jean",
+          role: "CLIENT",
+          phone_number: "222-222-2222",
+          password: "qwerty",
+          password_confirmation: "qwerty"
+        }
+      }
+    end
+
+    # code http
+    assert_response :success
+
+    # format reponse
+    json = JSON.parse(response.body)
+
+    # donne reponse
+    assert_equal "error", json["status"]
+    assert_equal 422, json["code"]
+    assert_includes json["errors"].to_s, "Email has already been taken"
   end
 end

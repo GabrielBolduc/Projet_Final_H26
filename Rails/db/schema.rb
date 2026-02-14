3cdb7a1fc6c8ebe746bbf41d9ad2bf86b55ff387
GabrielBolduc <bolducGabriel@outlook.com>
2026-02-14 13:38:20 -0500

    creation table Festival

--- Rails/db/schema.rb
+++ Rails/db/schema.rb
@@ -10,7 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_12_160635) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_14_182114) do
+  create_table "festivals", charset: "utf8mb4", collation: "utf8mb4_uca1400_ai_ci", force: :cascade do |t|
+    t.string "address", limit: 250, null: false
+    t.text "comment"
+    t.integer "coordinates", null: false
+    t.datetime "created_at", null: false
+    t.integer "daily_capacity", null: false
+    t.date "end_at", null: false
+    t.decimal "other_expense", precision: 10
+    t.decimal "other_income", precision: 10
+    t.integer "satisfaction", limit: 1
+    t.date "start_at", null: false
+    t.string "statut", default: "DRAFT", null: false
+    t.datetime "updated_at", null: false
+    t.check_constraint "`daily_capacity` > 0", name: "check_festivals_daily_capacity_positive"
+    t.check_constraint "`satisfaction` >= 0 and `satisfaction` <= 5", name: "check_festivals_satisfaction_range"
+    t.check_constraint "`start_at` <= `end_at`", name: "check_festivals_dates_chronology"
+    t.check_constraint "`statut` in ('DRAFT','ONGOING','COMPLETED')", name: "check_festivals_statut_enum"
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_uca1400_ai_ci", force: :cascade do |t|
     t.string "ability"
     t.datetime "created_at", null: false

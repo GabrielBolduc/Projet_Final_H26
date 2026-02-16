3faac26b8ebaf53f060f057bf91d1bbbab0c7b4d
laurent3010 <l4ur3ntb34upr3@gmail.com>
2026-02-16 08:39:49 -0500

    completion du guard

--- Angular/src/app/core/guards/restriction-login-guard.ts
+++ Angular/src/app/core/guards/restriction-login-guard.ts
@@ -1,16 +1,21 @@
+
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
-export const AuthGuard: CanActivateFn = (route, state) => {
+
+export const restrictionLoginGuard: CanActivateFn = (route, state) => {
+
   const auth = inject(AuthService);
   const router = inject(Router);
 
-  if(!auth.isLoggedIn()) {
-    router.navigate(['/login']);
-    return false;
+  if(auth.isLoggedIn()) {
+    if(localStorage.getItem('type') === 'admin') {
+      router.navigate(['/']);
+      return false;
+    }
+   
   }
-  
-  
+
   return true;
 };

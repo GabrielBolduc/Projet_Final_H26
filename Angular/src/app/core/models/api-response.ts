a44fc2abb42679c44686bf838a5684e25f481963
GabrielBolduc <bolducGabriel@outlook.com>
2026-02-17 10:28:46 -0500

    Fix controller festival & service festival

---
+++ Angular/src/app/core/models/api-response.ts
@@ -0,0 +1,11 @@
+export interface ApiResponse<T> {
+  status: 'success' | 'error';
+  data: T;
+  meta?: {
+    currentPage: number;
+    totalPages: number;
+    totalItems: number;
+  };
+  message?: string;
+  code?: number;   
+}

e62aea1fec9424e5ee3b63be52e724a11d37eb07
GabrielBolduc <bolducGabriel@outlook.com>
2026-02-19 13:03:47 -0500

    merge avec main

--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { Login } from './features/auth/login/login';
 import { Signup } from './features/auth/signup/signup'; 
 import { Notfound } from './core/notfound/notfound';
 import { Home } from './features/home/home';
-import { Ticketing } from '@features/GabrielR/ticketing/ticketing';
 import { Reservations } from '@features/alexandre/reservations/reservations';
 import { ReservationsForm } from '@features/alexandre/reservations-form/reservations-form';
 import { Accommodations } from '@features/alexandre/accommodations/accommodations';

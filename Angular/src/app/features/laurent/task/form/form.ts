58c66a80d57f841a93855ec43cbb861f2e672d1d
laurent3010 <l4ur3ntb34upr3@gmail.com>
2026-02-19 10:42:36 -0500

    le typescript est pret

--- Angular/src/app/features/laurent/task/form/form.ts
+++ Angular/src/app/features/laurent/task/form/form.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Task } from '@core/models/task';
+import { TaskService } from '@core/services/task.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TaskPayload } from '@core/models/task-payload';
+
 
 @Component({
   selector: 'app-form',
@@ -8,4 +13,71 @@ import { Component } from '@angular/core';
 })
 export class TaskFormComponent {
 
+   private taskService = inject(TaskService);
+
+   taskId: number |null=null;
+   isEditMode = false;
+
+   task = signal<Task | null>(null);
+
+   editTask: TaskPayload = new TaskPayload(
+        "Edited title",
+        "Edited description",
+        2,        // difficulty
+        3,        // priority
+        true      // reusable
+        );
+   
+  constructor(
+        private route: ActivatedRoute,
+        private router: Router
+      )
+      {}
+
+  ngOnInit() {
+           const idParam = this.route.snapshot.paramMap.get('id');
+
+            
+            const id = idParam ? Number(idParam) : null;
+
+            if (id) {
+                this.isEditMode = true;
+                this.taskId = Number(id);
+
+                this.taskService.getTask(id).subscribe(data =>{ 
+                    console.log('tache reçu : ', data)
+                    this.task.set(data)
+                    console.log('tache initialisé : ', this.task)
+                  });
+
+            }
+  }
+
+  save() {
+
+    
+       
+        if (this.isEditMode) {
+            // EDIT
+            this.taskService.updateTask(this.taskId, this.editTask).subscribe(success => {
+                if (!success) {
+                    console.log('Update failed');
+                    return;
+                }
+            });
+            this.router.navigate(['/taches']);
+            
+        } else {
+            // CREATE
+            this.taskService.createTask(this.editTask).subscribe(success => {
+                if (!success) {
+                    console.log('Update failed');
+                    return;
+                }
+   
+            }); 
+            
+            this.router.navigate(['/taches']);
+        }
+    }
 }

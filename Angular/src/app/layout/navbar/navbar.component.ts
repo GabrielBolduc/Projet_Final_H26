f3b5cc6489fde35bd58c2ca73d23c504e636f2e7
GabrielBolduc <bolducGabriel@outlook.com>
2026-02-24 19:30:45 -0500

    fix programmation navbar

--- Angular/src/app/layout/navbar/navbar.component.ts
+++ Angular/src/app/layout/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
@@ -7,6 +7,8 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { MatMenuModule } from '@angular/material/menu'; 
 import { AuthService } from '@core/services/auth.service';
 import { MatDividerModule } from '@angular/material/divider';
+import { firstValueFrom } from 'rxjs';
+import { FestivalService } from '@core/services/festival.service'; 
 
 @Component({
   selector: 'app-navbar',
@@ -23,9 +25,27 @@ import { MatDividerModule } from '@angular/material/divider';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'   
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   public auth = inject(AuthService);
   public translate = inject(TranslateService);
+  private festivalService = inject(FestivalService); 
+
+  ongoingFestivalId = signal<number | null>(null);
+
+  async ngOnInit() {
+    if (this.auth.currentUser()) { 
+      try {
+        const festivals = await firstValueFrom(this.festivalService.getFestivals());
+        const ongoing = festivals.find(f => f.status === 'ongoing');
+        
+        if (ongoing) {
+          this.ongoingFestivalId.set(ongoing.id);
+        }
+      } catch (error) {
+        console.error("Impossible de charger le festival en cours pour la navbar", error);
+      }
+    }
+  }
 
   toggleLanguage() {
     const current = this.translate.getCurrentLang(); 
@@ -33,9 +53,8 @@ export class NavbarComponent {
     this.translate.use(targetLang);
     localStorage.setItem('userLanguage', targetLang); 
   }
+  
   onLogout() {
     this.auth.logout();
   }
-
-
 }

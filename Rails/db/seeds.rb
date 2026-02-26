b6edc1324eb531d26900f55c02f6e6e080b57041
RacGab <2051697@cshawi.ca>
2026-02-25 20:33:51 -0500

    Enlever puts dans seeds

--- Rails/db/seeds.rb
+++ Rails/db/seeds.rb
@@ -489,8 +489,6 @@ images.each do |package, filename|
       filename: filename,
       content_type: content_type 
     )
-  else
-    puts "Image non trouvée : #{filename}"
   end
 end
 
@@ -500,11 +498,9 @@ unit_camp_low.save!
 unit_hotel_high.save!
 unit_hotel_mid.save!
 
-puts "\nTicketing seed summary (ongoing festival):"
 [p_general, p_daily, p_evening, p_daily_sold_out, p_evening_last_spots].each do |pkg|
   sold = pkg.tickets.where(refunded: false).count
   refunded = pkg.tickets.where(refunded: true).count
-  puts "- #{pkg.title}: sold=#{sold}/#{pkg.quota}, refunded=#{refunded}"
 end
 
 

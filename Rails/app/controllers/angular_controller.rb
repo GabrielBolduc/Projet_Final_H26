ff75ba8fc0c49afd2b110dd13d710271280b7eaa
GabrielBolduc <bolducGabriel@outlook.com>
2026-02-26 14:33:08 -0500

    Correction fallback

--- Rails/app/controllers/angular_controller.rb
+++ Rails/app/controllers/angular_controller.rb
@@ -1,4 +1,4 @@
-class AngularController < ApplicationController
+class AngularController < ActionController::Base
   def index
     render file: Rails.root.join("public", "index.html"), layout: false
   end

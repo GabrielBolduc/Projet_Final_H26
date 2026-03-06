4465d6cf04131156747b0e92ec9e2c3372e05926
RacGab <2051697@cshawi.ca>
2026-03-05 20:59:01 -0500

    total dans commande client (discount)

--- Rails/app/models/order.rb
+++ Rails/app/models/order.rb
@@ -3,4 +3,12 @@ class Order < ApplicationRecord
   has_many :tickets, dependent: :destroy
 
   validates :user, presence: true
+
+  def subtotal
+    tickets.sum(:price)
+  end
+
+  def total_price
+    [ subtotal - discount, 0 ].max
+  end
 end

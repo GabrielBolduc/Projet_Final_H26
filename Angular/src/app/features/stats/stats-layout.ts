490b070a2135aec25af689e6f96ee03735d3fd8a
GabrielBolduc <bolducGabriel@outlook.com>
2026-03-09 11:42:11 -0400

    fix icon page stats

--- Angular/src/app/features/stats/stats-layout.ts
+++ Angular/src/app/features/stats/stats-layout.ts
@@ -22,9 +22,9 @@ export class StatsLayoutComponent {
   public translate = inject(TranslateService);
 
   navLinks = [
-    { path: 'festival', label: "Festival", icon: 'event' },
+    { path: 'festival', label: "Festival", icon: 'festival' },
     { path: 'hebergement', label: 'Hébergement', icon: 'hotel' },
-    { path: 'billetterie', label: 'Billetterie', icon: 'confirmation_number' },
-    { path: 'tache', label: 'Tâches', icon: 'task_alt' }
+    { path: 'billetterie', label: 'Billetterie', icon: 'local_activity' },
+    { path: 'tache', label: 'Tâches', icon: 'assignment' }
   ];
 }
